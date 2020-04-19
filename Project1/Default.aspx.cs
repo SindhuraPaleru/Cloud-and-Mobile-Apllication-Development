a4db6b40805adb92dadd01e607e220512e891d98
Paleru Sindhura <51043047+SindhuraPaleru@users.noreply.github.com>
2020-04-19 23:41:20 +0530

    Create Default.aspx.cs

---
+++ Project1/Default.aspx.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+namespace Default
+{
+    public partial class Default : System.Web.UI.Page
+    {
+
+        CloudBlobContainer container;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            CloudStorageAccount sa = CloudStorageAccount.Parse(Microsoft.WindowsAzure.ServiceRuntime.RoleEnvironment.GetConfigurationSettingValue("mycon"));
+            CloudBlobClient client = sa.CreateCloudBlobClient();
+            container = client.GetContainerReference("photobucket");
+            container.CreateIfNotExists();
+            container.SetPermissions(new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Blob });
+            ImageGrid.DataSource = container.ListBlobs();
+            ImageGrid.DataBind();
+
+        }
+
+        protected void cmdUpload_Click(object sender, EventArgs e)
+        {
+            CloudBlockBlob blob = container.GetBlockBlobReference(ImageSelector.FileName);
+            blob.UploadFromStream(ImageSelector.FileContent);
+            Response.Redirect("Default.aspx");
+
+        }
+    }
+}
